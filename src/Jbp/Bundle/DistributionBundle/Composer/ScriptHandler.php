a2a58aa87977ce67bec62140ee8fe2b982655dbd
zhanghua <2533058383@qq.com>
2016-10-21 16:58:04 +0800

    modify composer

--- src/Jbp/Bundle/DistributionBundle/Composer/ScriptHandler.php
+++ src/Jbp/Bundle/DistributionBundle/Composer/ScriptHandler.php
@@ -12,9 +12,10 @@
 namespace Jbp\Bundle\DistributionBundle\Composer;
 
 use Symfony\Component\ClassLoader\ClassCollectionLoader;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\PhpExecutableFinder;
-use Composer\Script\CommandEvent;
+use Composer\Script\Event;
 use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler as base;
 
 /**
@@ -41,19 +42,23 @@ class ScriptHandler extends Base
      *
      * @param $event CommandEvent A instance
      */
-    public static function buildBootstrap(CommandEvent $event)
+    public static function buildBootstrap(Event $event)
     {
         $options = static::getOptions($event);
         foreach ($options['dm-apps'] as $config) {
-            $appDir = $config['app-dir'];
+            $bootstrapDir = $autoloadDir = $config['app-dir'];
 
-            if (!is_dir($appDir)) {
-                echo 'The symfony-app-dir ('.$appDir.') specified in composer.json was not found in '.getcwd().', can not build bootstrap file.'.PHP_EOL;
-                return;
+            if (static::useNewDirectoryStructure($options)) {
+                $bootstrapDir = $options['symfony-var-dir'];
+                if (!static::hasDirectory($event, 'symfony-var-dir', $bootstrapDir, 'build bootstrap file')) {
+                    break;
+                }
             }
-            $appDir = 'app';
+            if (!static::hasDirectory($event, 'symfony-app-dir', $autoloadDir, 'build bootstrap file')) {
+                break;
+            }
 
-            static::executeBuildBootstrap($appDir, $options['process-timeout']);
+            static::executeBuildBootstrap($event, $bootstrapDir, $autoloadDir, $options['process-timeout']);
         }
     }
 
@@ -62,19 +67,53 @@ class ScriptHandler extends Base
      *
      * @param $event CommandEvent A instance
      */
-    public static function clearCache(CommandEvent $event)
+    public static function clearCache(Event $event)
     {
         $options = static::getOptions($event);
         foreach ($options['dm-apps'] as $config) {
+            $consoleDir = $config['app-dir'];
+            if (!static::hasDirectory($event, 'symfony-app-dir', $consoleDir, 'execute command')) {
+                break;
+            }
+            if (null === $consoleDir) {
+                break;
+            }
+            $warmup = '';
+            if (empty($options['symfony-cache-warmup'])) {
+                $warmup = ' --no-warmup';
+            }
+            $warmup = ' --no-warmup';
 
-            $appDir = $config['app-dir'];
+            static::executeCommand($event, $consoleDir, 'cache:clear' . $warmup, $options['process-timeout']);
+        }
 
-            if (!is_dir($appDir)) {
-                echo 'The symfony-app-dir ('.$appDir.') specified in composer.json was not found in '.getcwd().', can not clear the cache.'.PHP_EOL;
+    }
+
+    /**
+     * Returns a relative path to the directory that contains the `console` command.
+     *
+     * @param Event  $event      The command event
+     * @param string $actionName The name of the action
+     *
+     * @return string|null The path to the console directory, null if not found.
+     */
+    protected static function getConsoleDir(Event $event, $actionName)
+    {
+        $options = static::getOptions($event);
+
+        if (static::useNewDirectoryStructure($options)) {
+            if (!static::hasDirectory($event, 'symfony-bin-dir', $options['symfony-bin-dir'], $actionName)) {
                 return;
             }
-            static::executeCommand($event, $appDir, 'cache:clear --no-warmup', $options['process-timeout']);
+
+            return $options['symfony-bin-dir'];
         }
+
+        if (!static::hasDirectory($event, 'symfony-app-dir', $options['symfony-app-dir'], 'execute command')) {
+            return;
+        }
+
+        return $options['symfony-app-dir'];
     }
 
     /**
@@ -89,27 +128,30 @@ class ScriptHandler extends Base
      *
      * @param $event CommandEvent A instance
      */
-    public static function installAssets(CommandEvent $event)
+    public static function installAssets(Event $event)
     {
         $options = static::getOptions($event);
         foreach ($options['dm-apps'] as $config) {
-            $appDir = $config['app-dir'];
+            $consoleDir = $config['app-dir'];
+
+            if (null === $consoleDir) {
+                break;
+            }
+
             $webDir = $config['web-dir'];
+
             $symlink = '';
-            if ($options['dm-assets-install'] == 'symlink') {
+            if ('symlink' == $options['symfony-assets-install']) {
                 $symlink = '--symlink ';
-            } elseif ($options['dm-assets-install'] == 'relative') {
+            } elseif ('relative' == $options['symfony-assets-install']) {
                 $symlink = '--symlink --relative ';
             }
 
-            if (!is_dir($webDir)) {
-                echo 'The symfony-web-dir ('.$webDir.') specified in composer.json was not found in '.getcwd().', can not install assets.'.PHP_EOL;
-
-                return;
+            if (!static::hasDirectory($event, 'symfony-web-dir', $webDir, 'install assets')) {
+                break;
             }
 
-            static::executeCommand($event, $appDir, 'assets:install '.$symlink.escapeshellarg($webDir));
-
+            static::executeCommand($event, $consoleDir, 'assets:install ' . $symlink . escapeshellarg($webDir), $options['process-timeout']);
         }
 
     }
@@ -119,8 +161,54 @@ class ScriptHandler extends Base
      *
      * @param $event CommandEvent A instance
      */
-    public static function installRequirementsFile(CommandEvent $event)
+    public static function installRequirementsFile(Event $event)
     {
+        $options = static::getOptions($event);
+        $sensioDistributionBundleDir =  dirname((new \ReflectionClass(get_parent_class(__CLASS__)))->getFileName());
+        foreach ($options['dm-apps'] as $config) {
+            $appDir = $config['app-dir'];
+            $fs = new Filesystem();
+
+            $newDirectoryStructure = static::useNewDirectoryStructure($options);
+
+            if (!$newDirectoryStructure) {
+                if (!static::hasDirectory($event, 'symfony-app-dir', $appDir, 'install the requirements files')) {
+                    break;
+                }
+                $fs->copy($sensioDistributionBundleDir . '/../Resources/skeleton/app/SymfonyRequirements.php', $appDir . '/SymfonyRequirements.php', true);
+                $fs->copy($sensioDistributionBundleDir . '/../Resources/skeleton/app/check.php', $appDir . '/check.php', true);
+            } else {
+                $binDir = $options['symfony-bin-dir'];
+                $varDir = $options['symfony-var-dir'];
+                if (!static::hasDirectory($event, 'symfony-var-dir', $varDir, 'install the requirements files')) {
+                    break;
+                }
+                if (!static::hasDirectory($event, 'symfony-bin-dir', $binDir, 'install the requirements files')) {
+                    break;
+                }
+                $fs->copy($sensioDistributionBundleDir . '/../Resources/skeleton/app/SymfonyRequirements.php', $varDir . '/SymfonyRequirements.php', true);
+                $fs->copy($sensioDistributionBundleDir . '/../Resources/skeleton/app/check.php', $binDir . '/symfony_requirements', true);
+                $fs->remove(array($appDir . '/check.php', $appDir . '/SymfonyRequirements.php', true));
+
+                $fs->dumpFile($binDir . '/symfony_requirements', '#!/usr/bin/env php' . "\n" . str_replace(".'/SymfonyRequirements.php'", ".'/" . $fs->makePathRelative($varDir, $binDir) . "SymfonyRequirements.php'", file_get_contents($binDir . '/symfony_requirements')));
+                $fs->chmod($binDir . '/symfony_requirements', 0755);
+            }
+
+            $webDir = $config['web-dir'];
+
+            // if the user has already removed the config.php file, do nothing
+            // as the file must be removed for production use
+            if ($fs->exists($webDir . '/config.php')) {
+                $requiredDir = $newDirectoryStructure ? $varDir : $appDir;
+
+                $fs->dumpFile($webDir . '/config.php', str_replace('/../app/SymfonyRequirements.php', '/' . $fs->makePathRelative($requiredDir, $webDir) . 'SymfonyRequirements.php', file_get_contents($sensioDistributionBundleDir . '/../Resources/skeleton/web/config.php')));
+            }
+        }
+
+
+
+
+        exit;
         $options = self::getOptions($event);
 
         foreach ($options['dm-apps'] as $config) {
@@ -149,7 +237,7 @@ class ScriptHandler extends Base
 
     }
 
-    protected static function getOptions(CommandEvent $event)
+    protected static function getOptions(Event $event)
     {
         $options = array_merge(static::$options, $event->getComposer()->getPackage()->getExtra());
         $options['process-timeout'] = $event->getComposer()->getConfig()->get('process-timeout');
