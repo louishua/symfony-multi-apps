ab0266983a213a9f30dce622a0136ca9ef1b7eda
zhanghua <2533058383@qq.com>
2016-10-22 14:36:44 +0800

    i18n

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,8 +14,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $translated = $this->get('translator')->trans('Symfony is great');
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
+            'sf'=>'Symfony is great',
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir')).DIRECTORY_SEPARATOR,
         ));
     }
