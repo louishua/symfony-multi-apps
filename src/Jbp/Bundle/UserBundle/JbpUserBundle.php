08f6f8c67b123e79c5161693e694ee08626b0484
boystar <814411929@qq.com>
2016-10-27 09:49:08 +0800

    更新

--- src/Jbp/Bundle/UserBundle/JbpUserBundle.php
+++ src/Jbp/Bundle/UserBundle/JbpUserBundle.php
@@ -4,7 +4,12 @@ namespace Jbp\Bundle\UserBundle;
 
 use Jbp\Bundle\UserBundle\Entity\JukuUser;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Jbp\Bundle\UserBundle\DependencyInjection\UnconventionalExtensionClass;
 
 class JbpUserBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new UnconventionalExtensionClass();
+    }
 }
