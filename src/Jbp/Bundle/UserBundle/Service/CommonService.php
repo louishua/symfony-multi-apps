b11aa0d34640673d6e98bbe874947c4bb28288bd
boystar <814411929@qq.com>
2016-10-28 09:35:36 +0800

    修改服务

--- src/Jbp/Bundle/UserBundle/Service/CommonService.php
+++ src/Jbp/Bundle/UserBundle/Service/CommonService.php
@@ -11,14 +11,30 @@ class CommonService
 
     public $doctrine;
 
-    public function __construct(KernelInterface $kernel)
-    {//$kernel = new UsercenterKernel('dev', true);
-        var_dump($kernel->getContainer()->get('userAccount_service'));exit;
-//        $this->doctrine = $doctrine;
+    protected $kernel;
+
+    public function __construct(KernelInterface $kernel,$doctrine)
+    {
+        $this->kernel = $kernel;
+        $this->doctrine = $doctrine;
     }
+
+    /**
+     * @return mixed
+     */
     public function getDoctrine()
     {
         return $this->doctrine;
+    }
+
+    /**
+     * 服务调用方法
+     * @param $id
+     * @return object
+     */
+    public function get($id)
+    {
+        return $this->kernel->getContainer()->get($id);
     }
 
 }
