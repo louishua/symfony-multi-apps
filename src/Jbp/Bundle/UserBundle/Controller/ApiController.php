5b9f6c52b26868cf21db0ff16d333c9cd0c19773
boystar <814411929@qq.com>
2016-10-28 15:35:01 +0800

    屏蔽注释

--- src/Jbp/Bundle/UserBundle/Controller/ApiController.php
+++ src/Jbp/Bundle/UserBundle/Controller/ApiController.php
@@ -11,23 +11,23 @@ class ApiController extends Controller
 {
 
     //接口调用服务Demo
-    public function mobileRegisterAction(Request $request)
-    {
-        $mobile = $request->request->get('mobile','5678');
-        $password = $request->request->get('password','22222');
-        $userService = $this->get('user_service');
-        $res = $userService->registerByUserMobile($mobile,$password);
-        if($res)
-        {
-            echo json_encode([
-                'error'=>0,
-                'msg'=>'ok',
-            ]);exit;
-        }else{
-            echo json_encode([
-                'error'=>1,
-                'msg'=>'fail',
-            ]);exit;
-        }
-    }
+//    public function mobileRegisterAction(Request $request)
+//    {
+//        $mobile = $request->request->get('mobile','5678');
+//        $password = $request->request->get('password','22222');
+//        $userService = $this->get('user_service');
+//        $res = $userService->registerByUserMobile($mobile,$password);
+//        if($res)
+//        {
+//            echo json_encode([
+//                'error'=>0,
+//                'msg'=>'ok',
+//            ]);exit;
+//        }else{
+//            echo json_encode([
+//                'error'=>1,
+//                'msg'=>'fail',
+//            ]);exit;
+//        }
+//    }
 }
