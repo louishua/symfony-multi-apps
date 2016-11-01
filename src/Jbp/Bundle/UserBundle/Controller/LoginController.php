17a5b98466ddda5332a5bb0a3ef968126268780c
boystar <814411929@qq.com>
2016-11-01 16:52:33 +0800

    提交

--- src/Jbp/Bundle/UserBundle/Controller/LoginController.php
+++ src/Jbp/Bundle/UserBundle/Controller/LoginController.php
@@ -2,11 +2,8 @@
 
 namespace Jbp\Bundle\UserBundle\Controller;
 
-use Doctrine\Common\Util\Debug;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Security;
 
 class LoginController extends Controller
 {
@@ -16,38 +13,25 @@ class LoginController extends Controller
      */
     public function indexAction(Request $request)
     {
-
         $curUser = $this->getUser();
         if ($curUser) {
             return $this->redirectToRoute("jbp_user_index");
         }
-
         $authenticationUtils = $this->get('security.authentication_utils');
-
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
-//        return $this->render(
-//            'JbpWebBundle:Security:login.html.twig',
-//            array(
-//                // last username entered by the user
-//                'last_username' => $lastUsername,
-//                'error'         => $error,
-//            )
-//        );
+        return $this->render('login/index.html.twig',[
+                'last_username' => $lastUsername,
+                'error'         => $error,
+        ]);
     }
 
     public function loginCheckAction()
     {
-
     }
 
     public function loginOutAction()
     {
-
     }
 
 }
