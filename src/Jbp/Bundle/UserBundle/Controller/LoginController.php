4a343010249815a624e5c32d04e6be214767fe17
boystar <814411929@qq.com>
2016-11-01 16:48:33 +0800

    ct

--- src/Jbp/Bundle/UserBundle/Controller/LoginController.php
+++ src/Jbp/Bundle/UserBundle/Controller/LoginController.php
@@ -3,13 +3,19 @@
 namespace Jbp\Bundle\UserBundle\Controller;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 =======
+=======
+>>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
 use Doctrine\Common\Util\Debug;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+<<<<<<< HEAD
+>>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
+=======
 >>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
 use Symfony\Component\Security\Core\Security;
 
@@ -22,6 +28,10 @@ class LoginController extends Controller
     public function indexAction(Request $request)
     {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
 =======
 
 >>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
@@ -29,6 +39,7 @@ class LoginController extends Controller
         if ($curUser) {
             return $this->redirectToRoute("jbp_user_index");
         }
+<<<<<<< HEAD
 <<<<<<< HEAD
         $authenticationUtils = $this->get('security.authentication_utils');
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -43,6 +54,8 @@ class LoginController extends Controller
     public function loginCheckAction()
     {
 =======
+=======
+>>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
 
         $authenticationUtils = $this->get('security.authentication_utils');
 
@@ -65,12 +78,19 @@ class LoginController extends Controller
     public function loginCheckAction()
     {
 
+<<<<<<< HEAD
+>>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
+=======
 >>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
     }
 
     public function loginOutAction()
     {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
 =======
 
 >>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
--- src/Jbp/Bundle/UserBundle/Controller/RegisterController.php
+++ src/Jbp/Bundle/UserBundle/Controller/RegisterController.php
@@ -3,13 +3,19 @@
 namespace Jbp\Bundle\UserBundle\Controller;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 =======
+=======
+>>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
 use Doctrine\Common\Util\Debug;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+<<<<<<< HEAD
+>>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
+=======
 >>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
 
 class RegisterController extends Controller
@@ -23,8 +29,13 @@ class RegisterController extends Controller
         $mobile = $request->query->get('mobile','1392299');
         $password = $request->query->get('password','123456');
 <<<<<<< HEAD
+<<<<<<< HEAD
         $userService = $this->get('saler_service');
         $reg = $userService->registerByMobile($mobile,$password);
+=======
+        $userService = $this->get('user_service');
+        $reg = $userService->registerByUserMobile($mobile,$password);
+>>>>>>> f60832038a874a1ed70371b062a904c2f42f68d5
 =======
         $userService = $this->get('user_service');
         $reg = $userService->registerByUserMobile($mobile,$password);
